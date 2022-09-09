4100180c14ba31a74bb1718a65d932089ea3c25f
jo <aingafitahina@gamil.com>
2022-09-09 14:08:23 +0200

    petite modif

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,57 +1,221 @@
-$(".button1").on({
-    mouseenter: function() {
-        $(this).css({
-            "opacity": "100%"
-        });
-    },
-    mouseleave: function() {
-        $(this).css({
-            "opacity" : "40%"
-        });
-    },
-});
+$(document).ready(function() {
+    let $win = $(window);
+    let $doc = $(document)
 
-$(".menu").on({
-    mouseenter: function() {
-        $(this).css({
-            "text-decoration": "underline",
-            "font-weight" : "bold"
-        });
-    },
-    mouseleave: function() {
-        $(this).css({
-            "text-decoration": "none",
-            "font-weight" : "normal"
-        });
-    },
-});
+    /* LES SLIDERS DE HOME */
+    /* scrollbar */
+    $win.scroll(function() {
+        if ($win.width() > 768) {
+            let $posY = $doc.scrollTop()
+            $(".scroll").css({ "transform": `translateY(${$posY}px)` })
+        }
+    });
 
-var i = 0;
-var images = ['Batslider1.png', 'Batslider2.png', 'Batslider3.png'];
-function slideImage(){
-    document.slider.src= "../img/Home/" + images[i];
-    if(i<images.length -1){
-        i++;
+    /* SLIDER AUTO */
+    $(function() {
+
+        /* slider 1*/
+        let $sliderItem = $(".slider-item");
+        let $now = 0;
+
+        function slide() {
+            for (var i = 0; i < $sliderItem.length; i++) {
+                $($sliderItem[i]).css('display', 'none')
+            }
+            $now++;
+            ($now > $sliderItem.length) ? ($now = 1) : {};
+            $($sliderItem[$now - 1]).fadeIn(1000)
+
+        }
+        /* slider 2 */
+        /* SLIDE CITATION */
+
+        let $citeIndex = 6;
+
+        function showCitation() {
+            let $i;
+            let $citationSlider = $(".citation-carte");
+            let $citationIndic = $(".indicator-item")
+            for ($i = 0; $i < $citationSlider.length; $i++) {
+                $($citationSlider[$i]).css('display', 'none');
+                $($citationIndic[$i]).hasClass("indicator-active") ? $($citationIndic[$i]).removeClass("indicator-active") : {};
+            }
+            $citeIndex++;
+            if ($citeIndex > $citationSlider.length) {
+                $citeIndex = 1;
+            }
+            $($citationSlider[$citeIndex - 1]).fadeIn(1000);
+            $($citationIndic[$citeIndex - 1]).addClass("indicator-active");
+        }
+
+        function autoSlide() {
+            slide();
+            showCitation()
+        }
+        showCitation()
+        slide()
+        setInterval(autoSlide, 3000)
+
+    });
+
+    /* MULTIMEDIA SLIDER */
+    let $showSlide = function($n) {
+        let $i;
+        let $mediaSlider = $(".slider-media");
+        if ($n > $mediaSlider.length) { $SlideIndex = 1 };
+        if ($n < 1) { $SlideIndex = $mediaSlider.length };
+        for ($i = 0; $i < $mediaSlider.length; $i++) {
+            $($mediaSlider[$i]).css('display', 'none')
+        }
+        $($mediaSlider[$SlideIndex - 1]).fadeIn(1500)
     }
-    else{
-        i=0
+
+    $("#multimedia button.left").click(function(e) {
+        e.preventDefault();
+        changeSlider(-1)
+
+    });
+
+    $("#multimedia button.right").click(function(e) {
+        e.preventDefault();
+        changeSlider(1)
+
+    });
+
+    let $SlideIndex = 1;
+    $showSlide($SlideIndex)
+
+    function changeSlider($n) {
+        $showSlide($SlideIndex += $n)
     }
-    setTimeout("slideImage()",2000);
-}
-window.onload-slideImage;
+    /* fin */
 
-// vers le haut
-let haut = document.querySelector('.icon1');
-haut.addEventListener('click', ()=>{
-    window.scrollTo({
-        top :0
-    })
-})
+    /* FIN SLIDER */
 
-//
-let bas = document.querySelector('.icon2');
-bas.addEventListener('click', ()=>{
-    window.scrollTo({
-        top :0
-    })
-})
+
+    /* Apparition au défillement */
+    const $options = { root: null, rootMargin: " 0px", threshold: 0.1 }
+    const $ratio = 0.1
+    $show = function(entries, $observer) {
+        entries.forEach($entry => {
+            if ($entry.intersectionRatio > $ratio) {
+                $($entry.target).addClass("show").slideDown(3000);
+                $observer.unobserve($entry.target);
+            }
+        });
+    }
+    const $observer = new IntersectionObserver($show, $options)
+    const $scrollShow = $(".scroll-show,.scroll-show > *")
+    for (let i = 0; i < $scrollShow.length; i++) {
+        $observer.observe($scrollShow[i])
+    }
+    $(function() { $("h2,#top .carte").animate({ 'left': '0' }, 1500) });
+
+
+
+    /* MENU DEROULANT */
+    const $menu = $(".menu .content"),
+        $menuBtn = $(".menu-btn"),
+        $menuList = $(".list");
+    const $optionsList = [
+        "Une fois par semaine",
+        "Une fois toutrs les deux semaine (2 fois par mois)",
+        "Une fois par mois ",
+        "Une fois touts les trois mois ",
+        "Une fois tous les six mois"
+    ];
+
+    function addOptions(selectedOption) {
+        $menuList.text("");
+        $optionsList.forEach(option => {
+            let isSelected = option == selectedOption ? "selected" : "";
+            let $li = `<li onclick="updateSelected(this)" class="${isSelected}">${option}</li>`;
+            $($li).appendTo($menuList);
+        });
+    }
+    addOptions();
+
+    updateSelected = function(selectedLi) {
+        addOptions($(selectedLi).text());
+        $menu.slideToggle(500);
+        $($menuBtn).find(":first-child").text($(selectedLi).text())
+    }
+    $menuBtn.on("click", () => $menu.slideToggle(500));
+
+
+
+
+
+
+    /* Les Carte */
+    $(".card-3>.col:not(.special)").hover(function() {
+        let $curent = $("img", this);
+        $curent.hasClass("zoomOut") ? $curent.removeClass("zoomOut").addClass("zoomIn") : $curent.addClass("zoomIn");
+        $(".info", this).toggle(500);
+
+    }, function() {
+        let $curent = $("img", this);
+        $curent.hasClass("zoomIn") ? $curent.removeClass("zoomIn").addClass("zoomOut") : $curent.addClass("zoomOut");
+        $(".info", this).toggle(500);
+    });
+
+    $(".special").hover(function() {
+        let $curent = $("img", this);
+        $curent.hasClass("zoomOut") ? $curent.removeClass("zoomOut").addClass("zoomIn") : $curent.addClass("zoomIn");
+        $(".info", this).toggle(500);
+
+    }, function() {
+        let $curent = $("img", this);
+        $curent.hasClass("zoomIn") ? $curent.removeClass("zoomIn").addClass("zoomOut") : $curent.addClass("zoomOut");
+        $(".info", this).toggle(500);
+    });
+
+
+    /* VALIDATION DES FORMULAIRES */
+    $(".form form").submit(function(e) {
+        e.preventDefault();
+        let $contact = $("#contact");
+        let $overlay = $("#overlay")
+        $overlay.css("height",`${$contact.height()}px`)
+        $contact.slideToggle(300)
+        $overlay.slideToggle(300);
+        let $formulaire = $("form")
+        let $entry = $($formulaire).find("input:not([type='submit']),textarea")
+        let $isValid = true;
+        for (let i = 0; i < $entry.length; i++) {
+            console.log($entry[i])
+            if ($($entry[i]).val().length == 0) {
+                $isValid = false
+            };
+        }
+        if ($isValid) {
+            $("#overlay .message").text("MESSAGE REÇU");
+
+            $("form").reset();
+        } else {
+            $("#overlay .message").text("VERIFIER LE FOMULAIRE, CAR IL YA DES CHAMPS NON REMPLIT")
+        }
+
+    });
+    $(".popcontainer").click(function(e) {
+        e.preventDefault();
+        $("#overlay").slideToggle(300);
+        $("#contact").slideToggle(300)
+    });
+
+
+
+
+    drawLine = function() {
+        if ($win.width() > 768) {
+            $('.lLine,.rLine').css("display", "block").css({ 'height': $("div.end").offset().top }, 'slow');
+        } else {
+            $('.lLine,.rLine,tline').css("display", "none")
+        }
+    }
+
+    $(document).resize(function() {
+        drawLine();
+    });
+    drawLine()
+});
